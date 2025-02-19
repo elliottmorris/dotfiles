444b2a7559c406ad5f31e64da42759aacfbd910f
G. Elliott Morris <11248148+elliottmorris@users.noreply.github.com>
2025-02-18 20:04:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,13 @@
 # We use Alacritty's default Linux config directory as our storage location here.
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+
+# isntall zsh 
+sudo apt install zsh
+
+# Make it your default shell: 
+chsh -s $(which zsh).
+
+# install powerlevel 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
